62c26f1e108f9ab6e6dd110cd6da0b0758fb58fd
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-02-01 14:31:05 +1300

    JS Basics

--- invert-regular-expression-matches.js
+++ invert-regular-expression-matches.js
@@ -1,2 +1,13 @@
+// Setup
+var testString = "How many non-space characters are there in this sentence?";
 
-49
+// Only change code below this line.
+
+var expression = /\S/g;  
+var notWhiteSpace = testString.match(expression).length; 
+// Change this line
+
+// Only change code above this line
+
+// This code counts the matches of expression in testString
+var nonSpaceCount = testString.match(expression).length;
